c189abd59e127779c058ad755e4320c450f400c0
Matej-02 <jaguarinho2@gmail.com>
2026-03-26 09:10:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,17 +12,24 @@ let roundCount = 0;
 let totalDistance = 0;
 let clickEnabled = true;
 
+// BEST SCORE
 let bestScore = localStorage.getItem("bestScore");
 if (bestScore !== null) {
   bestScore = Number(bestScore);
   document.getElementById("best").textContent = bestScore;
 }
 
+// LOAD GEOJSON
 fetch("locations.geojson")
   .then(res => res.json())
   .then(data => {
+    console.log("GeoJSON loaded");
     features = data.features;
     startRound();
+  })
+  .catch(err => {
+    console.error("NAPAKA GEOJSON:", err);
+    alert("Ne morem naložiti locations.geojson");
   });
 
 function startRound() {
@@ -34,37 +41,30 @@ function startRound() {
   clickEnabled = true;
 
   currentFeature = features[Math.floor(Math.random() * features.length)];
+
   alert("Najdi: " + currentFeature.properties.name);
 
-  // poslušaj oba dogodka
-  map.once("click", handleMapInteraction);
-  map.once("touchend", handleMapInteraction);
+  // ENOTEN EVENT (desktop + mobile)
+  map.once("pointerup", handleMapInteraction);
 }
 
 function handleMapInteraction(e) {
   if (!clickEnabled) return;
   clickEnabled = false;
 
-  let latlng;
-
-  // če je normalen klik
-  if (e.latlng) {
-    latlng = e.latlng;
-  } 
-  // če je touch event
-  else if (e.originalEvent && e.originalEvent.changedTouches) {
-    let touch = e.originalEvent.changedTouches[0];
-    latlng = map.mouseEventToLatLng(touch);
-  } else {
+  if (!e.latlng) {
+    console.log("Ni latlng!");
     return;
   }
 
-  onMapClick({ latlng });
+  onMapClick(e.latlng);
 }
 
-function onMapClick(e) {
-  let userPoint = turf.point([e.latlng.lng, e.latlng.lat]);
+function onMapClick(latlng) {
 
+  let userPoint = turf.point([latlng.lng, latlng.lat]);
+
+  // ROB POLIGONA
   let boundary = turf.polygonToLine(currentFeature);
 
   let nearest = turf.nearestPointOnLine(boundary, userPoint, {
@@ -80,24 +80,29 @@ function onMapClick(e) {
   document.getElementById("count").textContent = roundCount;
   document.getElementById("total").textContent = totalDistance;
 
+  // PRIKAŽI POLIGON
   let layer = L.geoJSON(currentFeature, {
     style: { color: "red", weight: 2 }
   }).addTo(map);
 
-  let userMarker = L.marker(e.latlng).addTo(map);
+  // USER MARKER
+  let userMarker = L.marker(latlng).addTo(map);
 
-  let nearestCoords = nearest.geometry.coordinates;
-  let nearestMarker = L.circleMarker([nearestCoords[1], nearestCoords[0]], {
+  // NEAREST POINT
+  let coords = nearest.geometry.coordinates;
+  let nearestMarker = L.circleMarker([coords[1], coords[0]], {
     radius: 6
   }).addTo(map);
 
+  // LINE
   let line = L.polyline([
-    e.latlng,
-    [nearestCoords[1], nearestCoords[0]]
+    latlng,
+    [coords[1], coords[0]]
   ]).addTo(map);
 
+  // POPUP
   L.popup()
-    .setLatLng(e.latlng)
+    .setLatLng(latlng)
     .setContent(
       "<b>" + currentFeature.properties.name + "</b><br>" +
       "Zgrešil si: <b>" + km + " km</b><br><br>" +
